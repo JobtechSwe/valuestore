c216181780b8f634867caeaac78b27a2b59e26c0
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-10-16 14:09:08 +0200

    Moved get_term and get_entity from elastic-importers into valuestore

--- valuestore/taxonomy.py
+++ valuestore/taxonomy.py
@@ -6,6 +6,7 @@ from elasticsearch.exceptions import RequestError
 log = logging.getLogger(__name__)
 
 ES_TAX_INDEX = os.getenv('ES_TAX_INDEX', 'taxonomy')
+taxonomy_cache = {}
 
 # Constants
 OCCUPATION = 'yrkesroll'
@@ -21,6 +22,7 @@ WORKTIME_EXTENT = 'arbetstidsomfattning'
 EMPLOYMENT_TYPE = 'anstallningstyp'
 DRIVING_LICENCE = 'korkort'
 
+# TODO: Check if taxtype and taxtype_legend can be combined
 tax_type = {
     OCCUPATION: 'jobterm',
     GROUP: 'jobgroup',
@@ -34,6 +36,25 @@ tax_type = {
     LANGUAGE: 'language',
     EMPLOYMENT_TYPE: 'employment_type',
     DRIVING_LICENCE: 'driving_licence'
+}
+taxtype_legend = {
+    'yrke': 'jobterm',
+    'yrkesroll': 'jobterm',
+    'yrkesgrupp': 'jobgroup',
+    'yrkesomrade': 'jobfield',
+    'sprak': 'language',
+    'kompetens': 'skill',
+    'kommun': 'municipality',
+    'lan': 'region',
+    'sprak': 'language',
+    'land': 'country',
+    'utbildningsinriktning': 'education',
+    'utbildningsniva': 'education_level',
+    'korkort': 'driving_licence',
+    'varaktighet': 'duration_type',
+    'lonetyp': 'wage_type',
+    'anstallningstyp': 'employment_type',
+    'arbetstidstyp': 'worktime_extent',
 }
 
 reverse_tax_type = {item[1]: item[0] for item in tax_type.items()}
@@ -57,7 +78,7 @@ def _build_query(query_string, taxonomy_code, entity_type, offset, limit):
     musts = []
     sort = None
     if query_string:
-        musts.append({"term": {"label.autocomplete": query_string}})
+        musts.append({"term": {"label": query_string}})
     else:
         offset = 0  # TODO parametrise offset and limit
         limit = 5000
@@ -92,6 +113,32 @@ def _build_query(query_string, taxonomy_code, entity_type, offset, limit):
     if sort:
         query_dsl['sort'] = sort
     return query_dsl
+
+
+def get_term(elastic_client, taxtype, taxid):
+    if taxtype not in taxonomy_cache:
+        taxonomy_cache[taxtype] = {}
+    if taxid in taxonomy_cache[taxtype]:
+        return taxonomy_cache[taxtype][taxid]
+    taxonomy_entity = get_entity(elastic_client, taxtype, taxid, {})
+    label = None
+    if 'label' in taxonomy_entity:
+        label = taxonomy_entity['label']
+    if 'term' in taxonomy_entity:
+        label = taxonomy_entity['term']
+    taxonomy_cache[taxtype][taxid] = label
+    return label
+
+
+def get_entity(elastic_client, taxtype, taxid, not_found_response=None):
+    doc_id = "%s-%s" % (taxtype_legend.get(taxtype, ''), taxid)
+    taxonomy_entity = elastic_client.get_source(index=ES_TAX_INDEX,
+                                                id=doc_id,
+                                                doc_type='_all', ignore=404)
+    if not taxonomy_entity:
+        log.warning("No taxonomy entity found for type %s and id %s" % (taxtype, taxid))
+        return not_found_response
+    return taxonomy_entity
 
 
 def find_concepts(elastic_client, query_string=None, taxonomy_code=None, entity_type=None,

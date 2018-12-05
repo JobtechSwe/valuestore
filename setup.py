2a8d6f8f1202d344f9960b2709309d2d51c5c22b
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-12-05 10:18:31 +0100

    Use find_packages to find packages

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='valuestore',
-    packages=['valuestore'],
+    packages=find_packages,
     include_package_data=True,
     install_requires=[
         'elasticsearch'
